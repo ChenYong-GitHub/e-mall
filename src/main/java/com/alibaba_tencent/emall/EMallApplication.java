8f929d88170dcd12808f227c9b63c43e137937b8
ChenYong <674556305@qq.com>
2020-01-03 14:45:10 +0800

    仿京东电商项目

---
+++ src/main/java/com/alibaba_tencent/emall/EMallApplication.java
@@ -0,0 +1,13 @@
+package com.alibaba_tencent.emall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alibaba_tencent/emall/EMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alibaba_tencent.emall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
