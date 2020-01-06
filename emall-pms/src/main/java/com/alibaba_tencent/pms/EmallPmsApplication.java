0cc5656456a367ca7f8ed36c7159e2331640617f
ChenYong <674556305@qq.com>
2020-01-06 22:09:30 +0800

    创建基础类

--- emall-pms/src/main/java/com/alibaba_tencent/pms/EmallPmsApplication.java
+++ emall-pms/src/main/java/com/alibaba_tencent/pms/EmallPmsApplication.java
@@ -1,8 +1,12 @@
 package com.alibaba_tencent.pms;
 
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.alibaba_tencent.pms.mapper")
+@EnableDubbo
 @SpringBootApplication
 public class EmallPmsApplication {
 
--- emall-pms/src/main/java/com/alibaba_tencent/pms/service/impl/AlbumPicServiceImpl.java
+++ emall-pms/src/main/java/com/alibaba_tencent/pms/service/impl/AlbumPicServiceImpl.java
@@ -1,10 +1,11 @@
 package com.alibaba_tencent.pms.service.impl;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.alibaba_tencent.emall.pms.entity.AlbumPic;
 import com.alibaba_tencent.pms.mapper.AlbumPicMapper;
 import com.alibaba_tencent.emall.pms.service.AlbumPicService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 /**
  * <p>
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Service;
  * @author ChenYong
  * @since 2020-01-06
  */
+@Component
 @Service
 public class AlbumPicServiceImpl extends ServiceImpl<AlbumPicMapper, AlbumPic> implements AlbumPicService {
 
