78d03f155a3bb43eb933c901bfdbae48b15a63a6
shiwenz <13926108685@163.com>
2020-01-06 17:30:33 +0800

    生成基础类

---
+++ emall-pms/src/test/java/com/alibaba_tencent/emall/pms/EmallPmsApplicationTests.java
@@ -0,0 +1,30 @@
+package com.alibaba_tencent.emall.pms;
+
+import com.alibaba_tencent.api.pms.entity.Product;
+import com.alibaba_tencent.api.pms.service.ProductService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * @ClassName: EmallPmsApplicationTests
+ * @Desc:
+ * @Author: ChenYong
+ * @Date: 2020-01-06 17:19
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmallPmsApplicationTests {
+
+    @Resource
+    private ProductService productService;
+
+    @Test
+    public void contextLoads() {
+        Product byId = productService.getById(1);
+        System.out.println(byId.getName());
+    }
+}
